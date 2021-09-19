c6154b1cd2c26fb32f8471cb8d11764b63f3215b
Konstantin Lemko <lemko99@bk.ru>
2021-09-19 13:19:13 +0700

    refactor: change burger menu  styles and scripts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const burger = document.querySelector('.burger');
+const overlay = burger.previousElementSibling;
 
 burger.addEventListener('click', function() {
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
+  this.previousElementSibling.classList.toggle('active');
+})
+
+overlay.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  burger.nextElementSibling.classList.toggle('active');
+  burger.previousElementSibling.classList.toggle('active');
 })
